54bbba7fc5140c7ecd70dbb0fccec54f9c9d50a7
shobro <54628243+shobro@users.noreply.github.com>
2020-01-13 19:37:04 +0530

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include <stdlib.h>
+typedef struct {
+    int data;
+    struct node *next
+}node;
+node * push(node *top){
+    node *temp=(node *)malloc(sizeof(node));
+    printf("Enter the data you want to store : ");
+    scanf(" %d",&temp->data);
+    if(top==NULL){
+    top=temp;
+    temp->next=NULL;
+    return top;
+    }
+    temp->next=top;
+    top=temp;
+    return top;
+}
+void display(node * top){
+    node *p=top;
+    while(p!=NULL){
+        printf("%d ",p->data);
+        p=p->next;
+    }
+    printf("\n");
+}
+node *pop(node *top){
+    node *temp=(node *)malloc(sizeof(node));
+    temp=top;
+    top=top->next;
+    free(temp);
+    return top;
+}
+int main(){
+node *top=NULL;
+int choice=1;
+while(choice){
+printf("Enter one to insert elements in the stack\nEnter two to display elements in the stack\nEnter three to pop elements : ");
+scanf(" %d",&choice);
+switch(choice){
+    case 1 :top=push(top);
+    break;
+    case 2 :display(top);
+    break;
+    case 3 :top=pop(top);
+    break;
+}
+}
+}
